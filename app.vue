99d1abff89c8d9990b6ca8f2fea9a2b6f8b71e1e
Areeb <areeb.hw@gmail.com>
2024-11-19 11:38:57 +0500

    new

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
-<script setup lang="ts">
-const router = useRouter()
-
-const nav = [
-  { label: 'Home', to: '/' },
-  { label: 'About', to: '/about' },
-  { label: 'Parent (index)', to: '/parent' },
-  { label: 'Parent (b)', to: '/parent/b' },
-  { label: 'Keyed child', onClick: () => router.push(`/parent/reload-${(Math.random() * 100).toFixed()}`) },
-  { label: 'Non-Keyed child', onClick: () => router.push(`/parent/static-${(Math.random() * 100).toFixed()}`) },
-]
-</script>
-
 <template>
-  <NuxtExample dir="routing/pages" :nav="nav" current-route>
-    <NuxtLoadingIndicator />
-    <NuxtPage />
-  </NuxtExample>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
